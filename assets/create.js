35d73803e4ed866b01f898ed453f15dd1abf6aac
Sofia Baldissera Pereira <135541865+sofia-balpe@users.noreply.github.com>
2025-01-10 12:58:44 -0300

    aterações dia 10

--- assets/create.js
+++ assets/create.js
@@ -1,7 +1,12 @@
+var pessoas = [
+    { name: "Nome", senha: "Senha", email: "Email" }
+]
 window.onload = () => {
 
+    //chamar a função para renderizar a tabela quando a página é recarregada
+    renderTable(); 
+    
     //Cabeçalho
-   
     var thead = document.querySelector('#tabela thead');
 
     keys = Object.keys(pessoas[0])
@@ -15,28 +20,53 @@ window.onload = () => {
     thead.appendChild(tr);
 
     //Células
-    var tbody = document.querySelector('#tabela tbody');
+    document.getElementById('idForm').addEventListener('submit', function (event) {
+        event.preventDefault();//Não vai recarregar pois está fora do contexto, aqui ela vai ficar add toda vez
 
-    pessoas.forEach(pessoa => {
+        var tdName = document.getElementById('idUser').value
+        var tdSenha = document.getElementById('idSenha').value
+        var tdEmail = document.getElementById('idEmail').value
 
-        var tr = document.createElement('tr')
+        var listaUsers = JSON.parse(localStorage.getItem('users'))
+        if (listaUsers == null) {
+            listaUsers = [];
+        }
 
-        var tdName = document.createElement('td')
-        tdName.textContent = pessoa.name
-        tr.appendChild(tdName);
+        user = {
+            nome: tdName,
+            senha: tdSenha,
+            email: tdEmail
+        }
 
-        var tdIdade = document.createElement('td')
-        tdIdade.textContent = pessoa.idade
-        tr.appendChild(tdIdade);
-
-        var tdData = document.createElement('td')
-        tdData.textContent = pessoa.data
-        tr.appendChild(tdData);
-
-
-        tbody.appendChild(tr);
+        listaUsers.push(user)
+        localStorage.setItem('users', JSON.stringify(listaUsers))
+        renderTable();
     })
 
-    
-    
+    //função para renderizar a tabela
+    function renderTable() { 
+        var tbody = document.querySelector('#tabela tbody');
+
+        tbody.innerHTML = "";//não deixa copiar o que tava escrito antes na tabela
+        console.log(tbody)
+
+        lista = JSON.parse(localStorage.getItem('users'))
+        lista.forEach(pessoa => {
+            var tr = document.createElement('tr')
+
+            var tdNome = document.createElement('td')
+            tdNome.textContent = pessoa.nome
+            tr.appendChild(tdNome)
+
+            var tdSenha = document.createElement('td')
+            tdSenha.textContent = pessoa.senha
+            tr.appendChild(tdSenha)
+
+            var tdEmail = document.createElement('td')
+            tdEmail.textContent = pessoa.email
+            tr.appendChild(tdEmail)
+
+            tbody.appendChild(tr);
+        })
+    }
 };
